5099663ec41100bd70daa1d3df5d6b24baebcedc
mdabdulkaderjoni <mdjoniemail@gmail.com>
2021-01-27 02:19:41 +0600

    css completed for both section

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+const enterBtn=document.getElementById('enter-btn');
+enterBtn.addEventListener('click',function(){
+    const loginArea=document.getElementById('login-area');
+    loginArea.style.display='none';
+    const mainArea=document.getElementById('main');
+    mainArea.style.display='block';
+
+    
+})
+
